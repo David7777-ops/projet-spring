09abe58743451c0721758357821f1d137bf354d2
David7777-ops <david.tavernier@sncf.fr>
2025-04-29 16:08:18 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,3 +11,15 @@ public class DemoApplication {
 	}
 
 }
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
